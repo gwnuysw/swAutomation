b8229b893cff9015124dcfa74f32ca87f0a9542d
seokwon <32347127+gwnuysw@users.noreply.github.com>
2020-06-07 21:17:48 +0900

    acme 로그인 성공 네이버 증권 데이터 가져오기 고전중

---
+++ csharpStudy/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharpStudy
+{
+    class Naver
+    {
+
+    }
+}
---
+++ csharpStudy/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace csharpStudy
+{
+    class Program
+    {
+        private IWebDriver driver = new ChromeDriver();
+        IWebElement ele = null;
+        static void Main(string[] args)
+        {
+            Program csharpPath = new Program();
+            //csharpPath.acmeLogin();
+            csharpPath.naverDataScrap();
+        }
+        
+        void naverDataScrap()
+        {
+            driver.Navigate().GoToUrl("https://finance.naver.com/marketindex/");
+            driver.Manage().Window.Maximize();
+            /*
+            ele = driver.FindElement(By.LinkText("증권"));
+            ele.Click();
+
+            ele = driver.FindElement(By.LinkText("시장지표"));
+            ele.Click();
+            */
+            IReadOnlyList<IWebElement> eles = driver.FindElements(By.CssSelector("span.date"));
+            Console.WriteLine(eles.Count);
+        }
+        void acmeLogin()
+        {
+            driver.Navigate().GoToUrl("https://acme-test.uipath.com/account/login");
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            ele = driver.FindElement(By.CssSelector("h1.page-header"));
+
+            ele.SendKeys("swlee1014@gmail.com");
+            ele = driver.FindElement(By.CssSelector("input#password.form-control"));
+            ele.SendKeys("1q2w3e!23");
+            ele = driver.FindElement(By.Id("buttonLogin"));
+
+            ele.Click();
+        }
+    }
+}
